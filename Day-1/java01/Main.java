9085d132c4fc56ef99eb3909d7a6e30e53b4c204
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-16 15:25:18 +0530

    Day-1 Learnings

---
+++ Day-1/java01/Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int a = 10;
+		int b = 20;
+		
+		String name = "Manoj";
+		
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase());
+		System.out.println(name.length());
+		System.out.println(name.charAt(0));
+		System.out.println(name.substring(0,3));
+		
+		System.out.println("Sum: " + (a+b));
+		System.out.println("Difference: " + (a-b));
+		System.out.println("Product: " + (a*b));
+		System.out.println("Quotient: " + (a/b));
+		System.out.println("Reminder: " + (a%b));
+		
+		
+	}
+}
