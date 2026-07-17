f7997b8b82f8d031cf3722986ab00e4cc3fede97
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 10:17:14 +0530

    Day-2 Learnings

---
+++ Day-2/relationalOperators.java
@@ -0,0 +1,12 @@
+public class relationalOperators {
+    public static void main(String[] args) {
+	    int a = 10;
+	    int b = 20;
+	    System.out.println(a>b);  // false
+	    System.out.println(a<b);  // true
+	    System.out.println(a==b); // false
+	    System.out.println(a!=b); // ture
+	    System.out.println(a>=b); // false
+	    System.out.println(a<=b); // true
+	}
+}
