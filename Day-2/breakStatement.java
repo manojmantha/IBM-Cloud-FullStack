56150a9bdca4ca6a4e02f9484172b08157a8f303
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 10:47:31 +0530

    Day-2 Learnings

---
+++ Day-2/breakStatement.java
@@ -0,0 +1,13 @@
+public class breakStatement {
+    public static void main(String[] args) {
+	    for(int i=1; i<=10; i++) {
+	        if(i==5) {
+	            System.out.println(i + " ");
+	            break;
+	        }
+	    }
+	    
+	    // output
+	    // 5
+	}
+}
