14d31892076d251a5520243097d1b276e11af798
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 11:17:23 +0530

    Day-2 Learnings

---
+++ Day-2/evenOrOdd.java
@@ -0,0 +1,14 @@
+public class evenOrOdd {
+    public static void main(String[] args) {
+	    int n = 7;
+	    
+	    if(n%2==0) {
+	        System.out.println("Even");
+	    } else{
+	        System.out.println("Odd");
+	    }
+	    
+	    // output
+	    // Odd
+	}
+}
