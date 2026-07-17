b7efeff3832e24dffba2985b35f0a44ccc96b249
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 10:19:15 +0530

    Day-2 Learnings

---
+++ Day-2/logicalOperators.java
@@ -0,0 +1,9 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+	    boolean a = true;
+	    boolean b = false;
+	    System.out.println(a && b); // false
+	    System.out.println(a || b); // true
+	    System.out.println(!a);     // false
+	}
+}
