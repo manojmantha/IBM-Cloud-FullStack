f6ace475cbfcd9b05852f6199a69a9b85521491e
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 10:43:56 +0530

    Day-2 Learnings

---
+++ Day-2/whileLoop.java
@@ -0,0 +1,16 @@
+public class whileLoop {
+    public static void main(String[] args) {
+	    int i = 1;
+	    while(i<=5){
+	        System.out.println("Count: " + i);
+	        i++;
+	    }
+	    
+	    // output
+	    // Count: 1
+        // Count: 2
+        // Count: 3
+        // Count: 4
+        // Count: 5
+	}
+}
