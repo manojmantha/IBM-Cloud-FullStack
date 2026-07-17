1f73e442c9a03db9a4888a328a03fc31da6463c8
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 14:43:17 +0530

    Day-2 Learnings

---
+++ Day-2/exercise3.java
@@ -0,0 +1,15 @@
+public class exercise3 {
+    public static void main(String[] args) {
+	    int a = 100;
+	    if(a%3==0){
+	        System.out.println("Fizz");
+	    } else if(a%5==0) {
+	        System.out.println("Buzz");
+	    } else {
+	        System.out.println(a);
+	    }
+	    
+	    // output
+	    // Buzz
+	}
+}
