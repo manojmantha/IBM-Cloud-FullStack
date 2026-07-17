974958029989f2fcdaba7b050756486c5b4b7504
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 10:23:38 +0530

    Day-2 Learnings

---
+++ Day-2/operatorsQuiz.java
@@ -0,0 +1,18 @@
+public class operatorsQuiz {
+    public static void main(String[] args) {
+	    // Question - 1
+	    int a = 5;
+	    int b = 2;
+	    System.out.println(a*b+a%b); // 11
+	    
+	    // Question - 2
+	    int x = 10;
+	    x += 5;
+	    x--;
+	    System.out.println(x); // 14
+	    
+	    // Question - 3
+	    boolean result = (7>3)&&(10==5+4);
+	    System.out.println(result); // false
+	}
+}
