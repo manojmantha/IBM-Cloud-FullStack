d72da591f3a93a5d11e84d872ad38541ca8464ea
Manoj Mantha <manojputtaluckey@gmail.com>
2026-07-17 11:15:38 +0530

    Day-2 Learnings

---
+++ Day-2/swapOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class swapOfTwoNumbers {
+    public static void main(String[] args) {
+	    int a = 5;
+	    int b = 10;
+	    int temp = a;
+	    a = b;
+	    b = temp;
+	    System.out.println("a=" + a + ", b=" + b);
+	    
+	    // output
+	    // a=10, b=5
+	}
+}
